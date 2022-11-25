5550a333f69c5ece934efefecba6e3fa3f2227fd
Jeremy Ottley <Gazaunga@gmail.com>
2022-11-24 20:59:02 -0500

    Update countWords.ml

--- countWords.ml
+++ countWords.ml
@@ -1,7 +1,8 @@
 let count (word: string): int =
 
-    let is_vowel = function
-        'a' | 'e' | 'i' | 'o' | 'u' | 'y' -> true
+    let is_vowel c = match c with
+        'a' | 'e' | 'i' | 'o' | 'u'
+        | 'A' | 'E' | 'I' | 'O' | 'U' -> true
         | _ -> false
 
     word |> String.to_seq |> Seq.filter (fun c -> is_vowel c) |> Array.of_seq
