2d1793514cdb7ff24115acf54b111ee8d46e15a6
Jeremy Ottley <Gazaunga@gmail.com>
2023-04-26 17:59:54 -0400

    Update pigLatin.ml

--- pigLatin.ml
+++ pigLatin.ml
@@ -1,12 +1,13 @@
-(* let to_pig_latin (word: string): string = *)
 
-let is_vowel = function
-  | 'a' | 'e' |'i' | 'o' | 'u'
-  | 'A' | 'E' | 'I' | 'O' | 'U' -> true
-  | _ -> false
+(* val to_pig_latin : string -> string *)
+let to_pig_latin (word: string): string =
 
-let pig_latin s =
-  let rest = String.sub s 1 (String.length s - 1) in
-  let first = String.make 1 s.[0] in
-  if (is_vowel s.[0]) then s ^ "yay" 
+  let is_vowel = function
+    | 'a' | 'e' |'i' | 'o' | 'u'
+    | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+    | _ -> false in
+
+  let rest = String.sub word 1 (String.length word - 1) in
+  let first = String.make 1 word.[0] in
+  if (is_vowel word.[0]) then word ^ "yay" 
   else rest ^ first ^ "ay"
