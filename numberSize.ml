bfc6a70686f84eedf6ae06daf418f171cbe017ea
Jeremy Ottley <Gazaunga@gmail.com>
2024-05-29 07:56:44 -0400

    Update numberSize.ml

--- numberSize.ml
+++ numberSize.ml
@@ -2,8 +2,8 @@ let small_word (s: string): bool =
   (String.length s) <= 4
 
 let big_or_small (s: string) = match s with
-  | s when smallWord(s) -> "Big!"
-  | s when not (smallWord(s)) -> "Small..."
+  | s when small_word(s) -> "Big!"
+  | s when not (small_word(s)) -> "Small..."
   | _ -> "Error"
 
 
